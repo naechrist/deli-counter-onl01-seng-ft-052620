a36e9c44e7a16ed6fb3966f7c78cd0c7627a0a7e
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-29 19:03:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.length >= 1 
+  final_sentence = "The line is currently:"
+  katz_deli.each_with_index { |name, index| final_sentence << " #{index + 1}. #{name}"}
+ puts final_sentence
+else
+  puts "The line is currently empty."
+end
+end
+
+def take_a_number(katz_deli, name)
+name = "Ada"
+counter = 1 
+return "Welcome, #{name}. You are number #{counter} in line."
+end
