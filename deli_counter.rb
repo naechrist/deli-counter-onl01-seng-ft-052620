e25fbd3f9835db00132eb02fb35c973554862a01
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-29 19:31:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
-  
+  puts "Currently serving #{katz_deli.shift}."
 puts "There is nobody waiting to be served!"
 end
